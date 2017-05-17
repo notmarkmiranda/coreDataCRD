66f2f5959e6f612bc6610214a748f0ce827e07b7
Mark Miranda <markmiranda@MacPro-102.lan>
2017-05-17 14:36:26 -0600

    initial commit

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -1,25 +1,70 @@
 //
 //  ViewController.swift
-//  todo
+//  ToDO App
 //
-//  Created by Mark Miranda on 5/16/17.
-//  Copyright © 2017 Mark Miranda. All rights reserved.
+//  Created by Bob Lee on 12/24/16.
+//  Copyright © 2016 BobtheDeveloper. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  @IBOutlet weak var tableView: UITableView!
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  
+  var tasks: [Task] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.delegate = self
+    tableView.dataSource = self
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  override func viewWillAppear(_ animated: Bool) {
+    getData()
+    tableView.reloadData()
   }
-
-
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tasks.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    
+    let task = tasks[indexPath.row]
+    
+    if let myName = task.name {
+      cell.textLabel?.text = myName
+    }
+    
+    return cell
+  }
+  
+  func getData() {
+    do {
+      tasks = try context.fetch(Task.fetchRequest())
+    }
+    catch {
+      print("Fetching Failed")
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      let task = tasks[indexPath.row]
+      context.delete(task)
+      (UIApplication.shared.delegate as! AppDelegate).saveContext()
+      
+      do {
+        tasks = try context.fetch(Task.fetchRequest())
+      }
+      catch {
+        print("Fetching Failed")
+      }
+    }
+    tableView.reloadData()
+  }
 }
 
